e01ea610207c2373d1e4d4841886d67e9225e68c
YoungAuthorj <106846129+YoungAuthorj@users.noreply.github.com>
2022-12-05 18:28:58 +0100

    Create 3-list_tables.sql

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,5 @@
+-- Write a script that lists all the tables of a database in your MySQL server.
+-- ===========================================================================
+--  The database name will be passed as argument of mysql command
+--  +(in the following example: mysql is the name of the database)
+SHOW TABLES;
